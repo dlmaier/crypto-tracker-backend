a8606513293965ade900a768071aeac1cd0170bf
dlmaier <93716698+dlmaier@users.noreply.github.com>
2025-05-26 14:17:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,11 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(3001, () => console.log("Server running on http://localhost:3001"));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 
 
 
